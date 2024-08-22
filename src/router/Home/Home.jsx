d1076ed2401ec0ea3913fa095480b29da2385aa9
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-08-22 21:36:56 +0500

    hali bolmadi

--- src/router/Home/Home.jsx
+++ src/router/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import Hero from '../../components/Hero/Hero'
 import Featured from '../../components/Featured/Featured'
-import Stayhome from '../../components/StayHome/Stayhome.jsx'
+
 import Collection from '../../components/Collection/Collection'
+import Stayhome from '../../components/Stayhome/Stayhome'
 
 const Home = () => {
     return (
